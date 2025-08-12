d319d50c86787e1cc9b3282a8a907b2ce03654f5
Vincenzo Pirozzi <vincenzo@example.com>
2025-08-12 18:44:09 +0000

    Rimuovi sezione 'Source Code' dalla navbar
    - Rimosso il link 'Source Code' dalla navbar del portfolio
    - Pulito il file delle costanti rimuovendo la sezione non necessaria

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -59,11 +59,7 @@ export const NAV_LINKS = [
     title: "Contact",
     link: null,
   },
-  {
-    id: "source-code",
-    title: "Source Code",
-    link: "http://www.github.com/sanidhyy/3d-portfolio/",
-  },
+
 ] as const;
 
 // Services
